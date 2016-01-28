564449761277c0913b54c55e486b7f53b291f0bb
Howard Yeh <howard@metacircus.com>
2016-01-28 12:51:35 +0800

    example project

---
+++ example/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {render} from "react-dom";
+
+// PostCSS support with autoprefixer.
+import "normalize.css";
+
+// CSS Module
+import style from "./App.css"
+
+// JSX support
+class App extends React.Component {
+  render() {
+    return <span className={style.hello}>hello quickpack</span>;
+  }
+}
+
+window.onload = () => {
+  render(<App/>,document.querySelector("#react-root"));
+};
+
