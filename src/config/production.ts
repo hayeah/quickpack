30c173f30ef1c234a1bae631663a2e3b9a514cb0
Howard Yeh <howard@metacircus.com>
2016-08-15 13:34:02 +0800

    add NODE_ENV=production for production build

--- src/config/production.ts
+++ src/config/production.ts
@@ -5,14 +5,20 @@ const UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
 
 import {WebpackConfig, QuickPackOptions} from "../options";
 
-export default function configProduction(config:WebpackConfig,options:QuickPackOptions): void {
+export default function configProduction(config: WebpackConfig, options: QuickPackOptions): void {
+
+  config.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': '"production"'
+    }
+  }));
   config.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
 
   config.plugins.push(new webpack.optimize.DedupePlugin());
 
   // uglify does not support ES6
   // https://github.com/mishoo/UglifyJS2/issues/448
-  if(!options.useES6 && options.useUglify) {
+  if (!options.useES6 && options.useUglify) {
     config.plugins.push(new UglifyJsPlugin());
   }
 }
